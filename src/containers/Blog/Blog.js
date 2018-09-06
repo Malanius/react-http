66a3abdcefc739fbefcb8330d1cb1114ac0f30df
Malanius <malanius@seznam.cz>
2018-09-06 19:33:24 +0200

    Transform received data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -16,13 +16,20 @@ class Blog extends Component {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
                 //console.log(response);
-                this.setState({ posts: response.data });
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return{
+                        ...post,
+                        author: 'Mal'
+                    }
+                });
+                this.setState({ posts: updatedPosts });
             });
     }
 
     render() {
         const post = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} />;
+            return <Post key={post.id} title={post.title} author={post.author}/>;
         });
 
         return (
